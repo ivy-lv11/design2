ee3cf479d59848c093429fa5612d2f406f536960
ivy-lv11 <181220038@smail.nju.edu.cn>
2020-04-04 21:27:45 +0800

    start

---
+++ cmake-build-debug/CMakeFiles/design.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design.dir/main.cpp.obj"
+  "design.exe"
+  "design.exe.manifest"
+  "design.pdb"
+  "libdesign.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
